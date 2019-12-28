c3f8a28a13be4890c593aa2d0b9f3123fdf8933e
linus muema <linus.m.muema@gmail.com>
2019-12-28 14:56:27 +0300

    Edit the UI components

--- app/src/main/java/com/moose/githublite/AppIntro.kt
+++ app/src/main/java/com/moose/githublite/AppIntro.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import com.github.paolorotolo.appintro.AppIntro2
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.OAuthCredential
 import com.google.firebase.auth.OAuthProvider
-import kotlinx.android.synthetic.main.slide_four.*
+import kotlinx.android.synthetic.main.slide_five.*
 
 
 class AppIntro : AppIntro2(){
@@ -26,12 +23,14 @@ class AppIntro : AppIntro2(){
         addSlide(SampleSlide.newInstance(R.layout.slide_two))
         addSlide(SampleSlide.newInstance(R.layout.slide_three))
         addSlide(SampleSlide.newInstance(R.layout.slide_four))
+        addSlide(SampleSlide.newInstance(R.layout.slide_five))
         showSkipButton(false)
         setProgressButtonEnabled(false)
 
     }
 
     fun btnClick(view: View) {
+        spinner.visibility = View.VISIBLE
         firebaseAuth.startActivityForSignInWithProvider(this, provider.build())
             .addOnSuccessListener{ result: AuthResult ->
                Log.d("success", result.credential.toString())
