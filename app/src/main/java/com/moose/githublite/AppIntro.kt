33c58f14b1081919a5dbadd791c7f923b9413051
linus muema <linus.m.muema@gmail.com>
2019-12-28 11:19:45 +0300

    Add Firebase SDK

--- app/src/main/java/com/moose/githublite/AppIntro.kt
+++ app/src/main/java/com/moose/githublite/AppIntro.kt
@@ -1,8 +1,9 @@
 package com.moose.githublite
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
 import com.github.paolorotolo.appintro.AppIntro2
+import kotlinx.android.synthetic.main.slide_four.*
 
 
 class AppIntro : AppIntro2(){
@@ -10,16 +11,25 @@ class AppIntro : AppIntro2(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //slides
         addSlide(SampleSlide.newInstance(R.layout.slide_one))
         addSlide(SampleSlide.newInstance(R.layout.slide_two))
         addSlide(SampleSlide.newInstance(R.layout.slide_three))
         addSlide(SampleSlide.newInstance(R.layout.slide_four))
         showSkipButton(false)
         setProgressButtonEnabled(false)
+
     }
 
-    fun btn_click() {
-        Toast.makeText(this, "Button click successful", Toast.LENGTH_SHORT).show()
+    fun btnClick(view: View) {
+        var username = name.text.toString()
+        if(username.equals(""))
+            name_layout.error = "Username cannot be empty"
+        else
+            githubAuth(username)
+    }
+
+    private fun githubAuth(username: String) {
+
     }
 }
+
--- app/src/main/java/com/moose/githublite/SampleSlide.kt
+++ app/src/main/java/com/moose/githublite/SampleSlide.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.slide_four.*
 
 
 class SampleSlide : Fragment() {
@@ -18,7 +17,8 @@ class SampleSlide : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(layoutResId, container, false)
+        val view: View = inflater!!.inflate(layoutResId, container, false)
+        return view
     }
 
     companion object {
