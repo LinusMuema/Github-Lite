9a2870ad148c8d2d944e9ae485112270a34cde79
linus muema <linus.m.muema@gmail.com>
2019-12-28 14:09:34 +0300

    Added firebase authentication

--- app/src/main/java/com/moose/githublite/AppIntro.kt
+++ app/src/main/java/com/moose/githublite/AppIntro.kt
@@ -1,12 +1,23 @@
 package com.moose.githublite
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.github.paolorotolo.appintro.AppIntro2
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthCredential
+import com.google.firebase.auth.OAuthProvider
 import kotlinx.android.synthetic.main.slide_four.*
 
 
 class AppIntro : AppIntro2(){
+
+    var provider = OAuthProvider.newBuilder("github.com")
+    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +32,21 @@ class AppIntro : AppIntro2(){
     }
 
     fun btnClick(view: View) {
-        var username = name.text.toString()
-        if(username.equals(""))
-            name_layout.error = "Username cannot be empty"
-        else
-            githubAuth(username)
+        firebaseAuth.startActivityForSignInWithProvider(this, provider.build())
+            .addOnSuccessListener{ result: AuthResult ->
+               Log.d("success", result.credential.toString())
+                updateUI()
+            }
+            .addOnFailureListener {
+                    exception ->
+                Log.d("failure",exception.toString())
+            }
     }
 
-    private fun githubAuth(username: String) {
-
+    private fun updateUI() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
